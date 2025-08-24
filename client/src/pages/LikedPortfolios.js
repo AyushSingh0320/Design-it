2c804685a6439f14947e5ee69bbb3dc4a4d54a1f
Ayush singh <aayushssingh2003@gmail.com>
2025-08-24 23:29:25 +0530

    manage collaboration

--- client/src/pages/LikedPortfolios.js
+++ client/src/pages/LikedPortfolios.js
@@ -74,7 +74,7 @@ const LikedPortfolios = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8 text-rose-100"> Portfolios that you have liked </h1>
+      <h1 className="text-3xl font-bold mb-8 text-rose-100"> Portfolios that you have Liked </h1>
       
       {/* Portfolio Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
