32b53d08c137611b843ea346a9aa2f6278c5d633
Ayush singh <aayushssingh2003@gmail.com>
2025-08-26 20:14:57 +0530

    add footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div className="sm:px-36 px-6 py-2.5 flex flex-col gap-5 justify-between md:flex-row text-secondary bg-gray-200 ">
+    <div className="sm:px-36 px-6 py-2.5 flex flex-col gap-5 justify-between md:flex-row text-secondary  ">
 
-      <div className="flex flex-col items-center font-semibold text-[17px]">
+      <div className="flex flex-col items-center font-semibold text-[17px] text-white">
         <p>Designed & Developed by</p>
         <div className="flex gap-1 items-center">
           <p>&nbsp;Ayush Singh</p>  <p>2025</p>
@@ -10,11 +10,11 @@ const Footer = () => {
       </div>
 
       <div className="gap-5 font-semibold tracking-wide flex flex-col md:flex-row items-center">
-        <div className="group flex flex-col justify-center cursor-pointer hover:text-blue-600">
+        <div className="group flex flex-col justify-center cursor-pointer text-white  hover:text-blue-600">
           <a href="https://github.com/Ayushsingh0320" target="_blank" rel="noreferrer">GITHUB</a>
           <div className="h-[1.5px] w-auto bg-secondary opacity-0 -translate-y-1 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0"></div>
         </div>
-        <div className="group flex flex-col justify-center cursor-pointer hover:text-blue-600 ">
+        <div className="group flex flex-col justify-center cursor-pointer text-white hover:text-blue-600 ">
           <a href="https://www.linkedin.com/in/ayush-singh-28a292369/" target="_blank" rel="noreferrer">LINKEDIN</a>
           <div className="h-[1.5px] w-auto bg-secondary opacity-0 -translate-y-1 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0"></div>
         </div>
