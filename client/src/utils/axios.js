24406fff748589cc39581af450d0e0e0ce014602
Ayush singh <aayushssingh2003@gmail.com>
2025-08-30 19:55:08 +0530

    production changes

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-  ?`${process.env.REACT_APP_API_URL}/api` || "https://design-it.live/api" : 'http://localhost:5000/api',
+  ? process.env.REACT_APP_API_URL || "https://design-it.live/api" : 'http://localhost:5000/api',
   withCredentials: true,
   timeout: 10000,
   headers: {
